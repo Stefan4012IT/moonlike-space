68d3ae9c3688dfe5001f5734e53afc62cf364f68
Stefan4012IT <42207092+Stefan4012IT@users.noreply.github.com>
2024-07-04 01:06:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './assets/styles/main.scss';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 
 function App() {
